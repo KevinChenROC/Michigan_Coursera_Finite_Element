ab8dc67e86b37ec3d6e13e4fa96a9ee999bd6ffc
kchen <kev888kev@gmail.com>
2025-10-08 01:43:35 +0200

    install script is fully ready

--- install_dealii.sh
+++ install_dealii.sh
@@ -4,8 +4,8 @@ sudo apt update
 sudo apt install -y \
   build-essential cmake gfortran pkg-config \
   wget curl tar xz-utils \
-  libblas-dev liblapack-dev \
-  libsuitesparse-dev libboost-all-dev \        # UMFPACK for SparseDirectUMFPACK
+  libblas-dev liblapack-dev libhdf5-dev \
+  libsuitesparse-dev libboost-all-dev \
   zlib1g-dev libbz2-dev
 #  ================================ download dealii without parallelization ================================
 
@@ -16,11 +16,15 @@ cd dealii-8.2.1
 
 mkdir build ; cd build
 
-# Create the math compatibility shim directly from the shell
-echo '#include <cmath>'                            >  dealii_math_compat.hpp
-echo 'using std::isnan;'                          >> dealii_math_compat.hpp
-echo 'using std::isinf;'                          >> dealii_math_compat.hpp
-echo 'using std::isfinite;'                       >> dealii_math_compat.hpp
+# one header to rule them all
+cat > dealii_compat.hpp <<'EOF'
+#include <cmath>
+#include <algorithm>
+#include <limits>
+using std::isnan;
+using std::isinf;
+using std::isfinite;
+EOF
 
 cmake .. \
   -DCMAKE_INSTALL_PREFIX=$HOME/local/dealii-8.2.1 \
@@ -31,7 +35,7 @@ cmake .. \
   -DDEAL_II_WITH_TBB=OFF \
   -DDEAL_II_WITH_PETSC=OFF \
   -DDEAL_II_WITH_TRILINOS=OFF \
-  -DDEAL_II_WITH_HDF5=OFF \
+  -DDEAL_II_WITH_HDF5=ON \
   -DDEAL_II_WITH_NETCDF=OFF \
   -DDEAL_II_WITH_UMFPACK=ON \
   -DDEAL_II_WITH_LAPACK=ON \
@@ -39,9 +43,11 @@ cmake .. \
   -DDEAL_II_WITH_ARPACK=OFF \
   -DCMAKE_CXX_STANDARD=11 \
   -DCMAKE_CXX_EXTENSIONS=ON \
-  -DCMAKE_CXX_FLAGS="-include $PWD/dealii_math_compat.hpp -include algorithm -include limits"
+  -DCMAKE_CXX_FLAGS="-include $PWD/dealii_compat.hpp" \
+  -DHDF5_INCLUDE_DIRS=/usr/include/hdf5/serial \
+  -DHDF5_LIBRARIES="/usr/lib/x86_64-linux-gnu/libhdf5_serial.so;/usr/lib/x86_64-linux-gnu/libhdf5_serial_hl.so" \
+  -DHDF5_IS_PARALLEL=OFF
 
-  # -DCMAKE_CXX_FLAGS="-std=gnu++11 -D_GLIBCXX_USE_C99_MATH_TR1 -include math.h"
-
-make -j8 
+make -j8
 make install & make test
+echo -e '\nexport DEAL_II_DIR=$HOME/local/dealii-8.2.1\nexport CMAKE_PREFIX_PATH=$DEAL_II_DIR:$CMAKE_PREFIX_PATH' >> ~/.zshrc
